a57b603c7aeadeb6b9c941c2c2b4e2f6c625a34e
bhavini1105 <bhavinipatel7532@gmail.com>
2025-05-03 15:17:16 +0530

    PR-1-REACT

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const savedCount = JSON.parse(localStorage.getItem('count')) || 0;
-    setCount(savedCount);
+    handlecount();
+    handleReset();
   }, []);
+
+  const handlecount =()=>{
+    const savedCount = JSON.parse(localStorage.getItem('count'));
+    setCount(savedCount);
+  }
+
+  const handleReset = ()=>{
+    const count = 0;
+    setCount(count);
+    localStorage.setItem('count',JSON.stringify(count));
+  }
 
   const handleDecrement = () => {
     if (count > 0) {
@@ -28,13 +39,13 @@ const App = () => {
       <h1>Counter App</h1>
       <div className="app-container">
         <div className="card">
-          <h2>🛍️ Stylish sneaker</h2>
-          <img src="https://images.unsplash.com/photo-1552346154-21d32810aba3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Product" className="product-image"  />
           <div className="counter-box">
             <button className="btn" onClick={handleDecrement}> - </button>
             <span className="count-display">{count}</span>
             <button className="btn" onClick={handleIncrement}> + </button>
           </div>
+
+            <button className='btn' onClick={handleReset}>RESET</button>
 
         </div>
       </div>
